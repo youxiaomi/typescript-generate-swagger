0ce8e6b796e1b16ff0008d1d091457525e4ce17e
squid <sq@sq.com>
2024-07-24 22:21:39 +0800

    fix:dev package and error message

--- packages/swagger-generate/ast-parser/src/createTsCheker.ts
+++ packages/swagger-generate/ast-parser/src/createTsCheker.ts
@@ -7,16 +7,22 @@ export const createTsChecker = (program:ts.Program)=>{
   let checker = program.getTypeChecker();
   let semanticDiagnostics = program.getSemanticDiagnostics()
   if (semanticDiagnostics.length) {
-    throw new Error(`semanticDiagnostics error  "${semanticDiagnostics[0].messageText}", please check semantic`)
+    getErrorMessage(semanticDiagnostics[0],'semanticDiagnostics')
   }
   let syntacticDiagnosticsSyncs = program.getSyntacticDiagnostics()
   if (syntacticDiagnosticsSyncs.length) {
-    throw new Error('syntacticDiagnosticsSyncs error '+ syntacticDiagnosticsSyncs[0].messageText)
+    getErrorMessage(semanticDiagnostics[0],'syntacticDiagnosticsSyncs')
   }
   let declarationDiagnostics = program.getDeclarationDiagnostics()
   if (declarationDiagnostics.length) {
-    let error =  new Error('declarationDiagnostics error '+ declarationDiagnostics[0].messageText)
-    throw error
+    getErrorMessage(semanticDiagnostics[0],'DeclarationDiagnostics')
   }
   return checker
+}
+
+function getErrorMessage(diagnostic:ts.Diagnostic,type:string) {
+  let { file, messageText, start } = diagnostic
+  const {line,character} = file.getLineAndCharacterOfPosition(start)
+  let text = `${type} error: ${messageText}, fileName:${file.fileName}:line:${line+1},chracter:${character+1}, please check `
+  throw new Error(text)
 }
